0e5443d0d0c7c87b421691e02a00dd59533241c1
Nicolette Edevbaro <nicolette.edevbaro@hotmail.com>
2022-11-22 23:20:26 +0200

    Made changes to level_test.py

--- src/tests/level_test.py
+++ src/tests/level_test.py
@@ -1,34 +1,34 @@
 import unittest
 from level import Level
 
-LEVEL_MAP_1 = [[0, 0, 0, 0, 0, 0, 0],
+testing_level = [[0, 0, 0, 0, 0, 0, 0],
                [0, 0, 0, 0, 0, 0, 0],
                [0, 0, 0, 0, 0, 0, 0],
                [0, 0, 0, 0, 0, 0, 0]]
 
-CELL_SIZE = 30
+testin_cell = 30
 created_blocks = {(1,1):(255, 0, 0), (1,2): (0,255,255), (1,3): (0,255,255)}
 
 
 
 class TestLevel(unittest.TestCase):
     def setUp(self):
-        self.level_1 = Level(LEVEL_MAP_1, CELL_SIZE)
+        self.testing_level = Level(testing_level, testin_cell)
 
 
     def test_block_can_move(self):
-        block = self.level_1.current_block
+        block = self.testing_level.current_block
 
         self.assertEqual(block.rect.x, 0)
         self.assertEqual(block.rect.y, 0)
 
 
-        self.level_1.move_block(dy=-CELL_SIZE)
-        self.assertEqual(block.rect.y, -CELL_SIZE)
+        self.testing_level.move_block(dy=-30)
+        self.assertEqual(block.rect.y, -30)
         self.assertEqual(block.rect.x, 0)
 
 
-        self.level_1.move_block(dx=-CELL_SIZE)
-        self.assertEqual(block.rect.x, -CELL_SIZE)
-        self.assertEqual(block.rect.y, -CELL_SIZE)
+        self.testing_level.move_block(dx=-30)
+        self.assertEqual(block.rect.x, -30)
+        self.assertEqual(block.rect.y, -30)
 
