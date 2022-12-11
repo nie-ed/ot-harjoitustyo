b42a1afe764a20070261e1f1651e3435ea2a0e8b
Nicolette Edevbaro <nicolette.edevbaro@hotmail.com>
2022-12-11 16:20:05 +0200

    Created map.py to create map on screen

---
+++ src/map.py
@@ -0,0 +1,41 @@
+import pygame
+
+class Map():
+    def __init__(self):
+        self.LEVEL_MAP = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+                          
+        self.CELL_SIZE = 30
+
+
+        height = len(self.LEVEL_MAP)
+        width = len(self.LEVEL_MAP[0])
+
+        _display_height = height * self.CELL_SIZE
+        _display_width = width * self.CELL_SIZE
+
+        display = pygame.display.set_mode((_display_width, _display_height))
+        
+        pygame.display.set_caption("Tetris")
+
+        display.fill((0, 0, 0))
+
+        self.display = display
